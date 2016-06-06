58f7d32847802bba7c96ba8172de4f36fbc4e7e5
Dan Becker <dabecker@paypal.com>
2016-06-06 13:47:53 -0500

    Demonstrated multiple filter predicates

--- src/main/java/com/example/lambda/StreamFilter.java
+++ src/main/java/com/example/lambda/StreamFilter.java
@@ -1,26 +1,38 @@
 package com.example.lambda;
 
+import java.util.Arrays;
 import java.util.function.Predicate;
 import java.util.stream.Stream;
 
 /**
  * This class can perform filtering on streams using an array of predicates.
-    Predicate<Person> allDrivers = p -> p.getAge() >= 16;
-    Predicate<Person> allDraftees = p -> p.getAge() >= 18 && p.getAge() <= 25 && p.getGender() == Gender.MALE;
-    Predicate<Person> allPilots = p -> p.getAge() >= 23 && p.getAge() <= 65;
-    Predicate<Person> allRetirees = p -> p.getAge() >= 62;
-    
+ * <p>
+ * This page has lots of help https://docs.oracle.com/javase/tutorial/java/javaOO/lambdaexpressions.html   
+ *     
  * @author <a href="mailto:dan@danbecker.info">Dan Becker</a>
  */
 public class StreamFilter {
     
-    // My generic syntax foo is low. Will fix this laters
-//    public Stream<T> filter( Stream<T> given, Predicate<? super T>... predicates ) {
-//        Stream<T> returned = Stream.empty();
-//        // for ( Predicate<? super T> predicate : predicates ) { 
-//        // }
-//        returned = given.stream()
-//                .filter(predicate);
-//        return returned;
-//    }
+    /** 
+     * This method performs a number of filter predicates on the given stream.
+     * <p>
+     * Example:
+     * <pre>
+     * List<Person> generated = StreamFilter.filter( pl.stream(), 
+     *     (Person p) -> p.getAge() >= 18, (Person p) -> p.getAge() <= 25, (Person p) -> p.getGender() == Gender.MALE)
+     *      .collect(Collectors.toList());
+     * </pre>
+     * @param given
+     * @param predicates
+     * @return
+     */
+    @SafeVarargs // gets rid of large list warning.
+    public static <T> Stream<T> filter( Stream<T> given, Predicate<? super T>... predicates ) {
+        Stream<T> returned = Stream.empty();
+        // http://stackoverflow.com/questions/24553761/how-to-apply-multiple-predicates-to-a-java-util-stream
+        Predicate<? super T> onePredicate = Arrays.stream(predicates).reduce(Predicate::and).orElse(t->true);
+        returned = given
+           .filter(onePredicate);
+        return returned;
+    }
 }
--- src/test/java/com/example/lambda/StreamFilterTest.java
+++ src/test/java/com/example/lambda/StreamFilterTest.java
@@ -1,9 +1,9 @@
 package com.example.lambda;
 
-import static org.junit.Assert.*;
 import java.util.List;
 import java.util.stream.Collectors;
 import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 
 public class StreamFilterTest {
 
@@ -12,18 +12,34 @@ public class StreamFilterTest {
 		List<Person> pl = Person.createShortList();
 		SearchCriteria search = SearchCriteria.getInstance();
 
+        // Predicate<Person> allDraftees = p -> p.getAge() >= 18 && p.getAge() <= 25 && p.getGender() == Gender.MALE;
 		List<Person> expected = pl.stream()
 		    .filter(search.getCriteria("allDraftees"))
 		    .collect(Collectors.toList());
-
-		// The idea is to pass an array/list of predicates and get a filtered stream back.
-//		List<Person> generated = StreamFilter.filter( pl.stream(), 
-//		    p -> p.getAge() >= 18, p.getAge() <= 25, p.getGender() == Gender.MALE)
-//		   .collect(Collectors.toList());
-
-		// Placeholder hack
+		List<Person> generated = StreamFilter.filter( pl.stream(), 
+		    (Person p) -> p.getAge() >= 18, (Person p) -> p.getAge() <= 25, (Person p) -> p.getGender() == Gender.MALE)
+		   .collect(Collectors.toList());
         assertEquals( "count", 2, expected.size());
-        // What is wanted
-        // assertEquals( "count", expected.size(), generated.size());
+        assertEquals( "count", expected.size(), generated.size());
+        
+        // Predicate<Person> allPilots = p -> p.getAge() >= 23 && p.getAge() <= 65;
+        expected = pl.stream()
+           .filter(search.getCriteria("allPilots"))
+           .collect(Collectors.toList());
+        generated = StreamFilter.filter( pl.stream(), 
+            (Person p) -> p.getAge() >= 23, (Person p) -> p.getAge() <= 65 )
+            .collect(Collectors.toList());
+        assertEquals( "count", 4, expected.size());
+        assertEquals( "count", expected.size(), generated.size());
+        
+        // Predicate<Person> allDrivers = p -> p.getAge() >= 16;
+        expected = pl.stream()
+           .filter(search.getCriteria("allDrivers"))
+           .collect(Collectors.toList());
+        generated = StreamFilter.filter( pl.stream(), 
+            (Person p) -> p.getAge() >= 18 )
+            .collect(Collectors.toList());
+        assertEquals( "count", 7, expected.size());
+        assertEquals( "count", expected.size(), generated.size());
 	}
 }
